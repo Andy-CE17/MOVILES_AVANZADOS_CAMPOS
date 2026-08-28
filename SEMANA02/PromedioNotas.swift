45853f15eb249029937fc641fb1435af735a07e3
Andy Luis Campos Escandon <andy.campos@tecsup.edu.pe>
2026-08-27 21:45:51 -0500

    Segunda parte: ingreso de alumno y notas

--- SEMANA02/PromedioNotas.swift
+++ SEMANA02/PromedioNotas.swift
@@ -4,3 +4,19 @@ import Foundation
 let exParcial = 0.3
 let trabajo = 0.3
 let exFinal = 0.4
+
+// Ingreso de datos
+print("Nombre del Alumno:")
+let alumno = readLine() ?? ""
+
+// Nota del examen parcial
+print("Nota del Examen Parcial:")
+let notaParcial = Double(readLine() ?? "") ?? 0.0
+
+// Nota del trabajo
+print("Nota del Trabajo:")
+let notaTrabajo = Double(readLine() ?? "") ?? 0.0
+
+// Nota del examen final
+print("Nota del Examen Final:")
+let notaFinal = Double(readLine() ?? "") ?? 0.0
